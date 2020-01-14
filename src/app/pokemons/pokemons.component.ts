dcc2ed36d13137bebda5028feba274e3af7f25f3
velrino <hi@velrino.com>
2020-01-14 01:32:56 -0300

    :feature: adding firebase list products and view product

--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -1,20 +1,40 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap';
+import { FirebaseDatabaseService } from '../shared/services/firebase.service';
+import { Observable } from 'rxjs';
+
+export interface PokemonsComponentInferface {
+  product: any,
+  products: Observable<any>,
+}
+
 @Component({
   selector: 'app-pokemons',
   templateUrl: './pokemons.component.html',
   styleUrls: ['./pokemons.component.css']
 })
+
 export class PokemonsComponent implements OnInit {
+  data: PokemonsComponentInferface = {
+    products: null,
+    product: null
+  };
+  contatos: Observable<any>;
 
   modalRef: BsModalRef;
-  constructor(private modalService: BsModalService) { }
+  constructor(private modalService: BsModalService, private db: FirebaseDatabaseService) { }
 
   ngOnInit() {
-
+    this.getAllProducts()
   }
 
-  openModal(template: TemplateRef<any>) {
+  getAllProducts() {
+    this.data.products = this.db.list('products');
+  }
+
+  openModal(template: TemplateRef<any>, item) {
+    console.log(item)
+    this.data.product = item;
     this.modalRef = this.modalService.show(template);
   }
 }
