6d5ffdb3f3728a6712cde487e849866fa680c892
velrino <hi@velrino.com>
2020-01-14 00:29:13 -0300

    :structure: adding firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +26,18 @@ import { ModalModule } from 'ngx-bootstrap/modal';
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(ROUTES),
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAwebJJFaXjPikRePAMywHaSKZf-4znaGQ",
+      authDomain: "pokemon-store.firebaseapp.com",
+      databaseURL: "https://pokemon-store.firebaseio.com",
+      projectId: "pokemon-store",
+      storageBucket: "pokemon-store.appspot.com",
+      messagingSenderId: "159590955979",
+      appId: "1:159590955979:web:55faf23b71373c2ae0944e",
+      measurementId: "G-GWXNXSEQW0"   
+    }, 'pokemons'),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
